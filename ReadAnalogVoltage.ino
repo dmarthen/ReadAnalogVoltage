5de83381a53c30800384c006c72a1948899f6416
Donny Marthen Sitompul <donny@donnymsitompul.com>
2018-01-16 17:40:11 +0700

    Add files via upload

---
+++ ReadAnalogVoltage.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(2);
+  float voltage = sensorValue * (5.0/1023.0);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  Serial.println(voltage); 
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);    
+  sensorValue = analogRead(2);
+  voltage = sensorValue * (5.0/1023.0);
+  Serial.println(voltage); 
+  delay(500);                  
+}
